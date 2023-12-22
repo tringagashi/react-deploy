f9de151b4720676f84eec1f0c7209301f83eb550
tringagashi <tringaagashi@gmail.com>
2023-12-22 17:29:01 +0100

    Versioni 2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Projekti MERN</h1>
+      <h1>Update 1</h1>
+     
+    </div>
+  );
+}
+
+export default App;
